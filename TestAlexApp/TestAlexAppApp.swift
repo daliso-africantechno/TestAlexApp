cbc5405246d3160fecc3a599b2a015adb8d0f916
Daliso Ngoma <daliso@africantechno.com>
2024-11-18 17:57:09 +0200

    Initial Commit

---
+++ TestAlexApp/TestAlexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAlexAppApp.swift
+//  TestAlexApp
+//
+//  Created by Daliso Ngoma on 2024/11/18.
+//
+
+import SwiftUI
+
+@main
+struct TestAlexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
